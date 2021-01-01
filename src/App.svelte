7a1ee58b759de478a0caea926b130bb1f13a1bdc
Andrea Giacobino <no.andrea@gmail.com>
2021-01-01 23:40:13 +0100

    ready for the prime time

--- src/App.svelte
+++ src/App.svelte
@@ -22,11 +22,27 @@
 
   </div>
 
-  <a
-    class="font-mono text-sm mt-2"
-    href="https://github.com/noandrea/costoflife-rs">
-    github.com/noandrea/costoflife-rs
-  </a>
+  <p class="my-2">
+    This is the CostOf.Life calculator (
+    <a
+      href="https://www.investopedia.com/terms/w/workinprogress.asp#:~:text=The%20term%20work%2Din%2Dprogress,stages%20of%20the%20production%20process.">
+      WIP
+    </a>
+    )
+  </p>
+  <p class="mb-2 text-center">
+    Currently the CostOf.Life calculator is available only as a
+    <a href="https://github.com/noandrea/costoflife-rs" class="underline">
+      command line program
+    </a>
+    for linux, but it will be made available on this page as soon as possible.
+  </p>
+  <p class="mb-2 text-center">
+    In the meantime you can read about it
+    <a href="https://dev.to/noandrea" class="underline">here</a>
+    .
+  </p>
+
   <Calculator />
 
 </main>
@@ -35,6 +51,15 @@
 <footer class="h-10 font-mono text-xs">
   by
   <a class="underline" href="https://adgb.me">adgb</a>
+
+  <span class="ml-2 pl-2 border-l border-costoflife-light-blue">sources</span>
+  <a href="https://github.com/noandrea/costoflife-rs" class="underline">
+    costoflife-rs
+  </a>
+  /
+  <a href="https://github.com/noandrea/costoflife.ui" class="underline">
+    costoflife.ui
+  </a>
   <span class="ml-2 pl-2 border-l border-costoflife-light-blue">see also</span>
   <a class="underline" href="https://meetvalis.com">VALIS</a>
 </footer>
--- src/Calculator.svelte
+++ src/Calculator.svelte
@@ -1,8 +1,9 @@
 <script>
   import { costoflife_per_diem } from "costoflife";
+
   // what is going on
   let raw = "";
-  let perDiem = -1;
+  let perDiem = null;
 
   function handleKeydown(event) {
     // console.log("Hello ", costoflife_greetings());
@@ -10,8 +11,10 @@
       console.log("key pressed: ", raw);
       // this is enter
       // perDiem = costoflife_per_diem(raw);
+      const redirectURL = "https://dev.to/noandrea";
       const x = raw;
-      const pm = costoflife_per_diem(x);
+      // const pm = module.costoflife_per_diem(x);
+      perDiem = `Deeply sorry, it is not yet ready, <a href="${redirectURL}" class="underline">here's why</a>`;
       console.log("key pressed: ", pm);
     }
   }
@@ -19,7 +22,7 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<div class="">
+<div class="text-center">
   <input
     bind:value={raw}
     type="text"
@@ -30,9 +33,11 @@
     focus:border-costoflife-light-blue text-base outline-none text-gray-700 py-1
     px-3 leading-8 transition-colors duration-200 ease-in-out" />
 
-  <div class="text-center">
-    {#if perDiem >= 0}
-      <p class="text-lg">{perDiem}€</p>
+  <div>
+    {#if perDiem}
+      <p class="text-lg">
+        {@html perDiem}
+      </p>
     {:else}
       <p>O_o</p>
     {/if}
